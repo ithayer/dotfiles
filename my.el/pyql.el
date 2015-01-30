31962440e72341359c30d609c10307bdde43d41e
Ignacio Thayer <ignacio@sake.(none)>
2015-01-30 08:40:12 -0800

    .

---
+++ my.el/pyql.el
@@ -0,0 +1,8 @@
+;; Provide a mode that helps to write logging statements that include
+;; all the local context and function arguments.
+;; Refactor as defun
+;; Refactor as variable
+;; Movement
+
+(provide 'pyql)
+
